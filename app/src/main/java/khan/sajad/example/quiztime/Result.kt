b80e6ab227a02bc44d5c3da9d0547a8be97ce7bc
sajadkhan <khansajad1910@outlook.com>
2022-09-19 14:52:25 +0530

    added the restart button

--- app/src/main/java/khan/sajad/example/quiztime/Result.kt
+++ app/src/main/java/khan/sajad/example/quiztime/Result.kt
@@ -1,8 +1,8 @@
 package khan.sajad.example.quiztime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import khan.sajad.example.quiztime.databinding.ActivityResultBinding
 
 class Result : AppCompatActivity() {
@@ -16,5 +16,10 @@ class Result : AppCompatActivity() {
         val maxScore = intent.extras?.getString("maxScore")
         binding.tvCongratulate.text = getString(R.string.congratulate, name)
         binding.tvScore.text = getString(R.string.score, score,maxScore)
+        binding.btnRestart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
